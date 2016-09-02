c084f21d4e9a5a3a6f593f4ce6b4223b32e46720
wewchan <chenv7@CNENCHENV7L1C.corp.emc.com>
2016-09-02 17:33:57 +0800

    Disable evil-mode by default

--- init.el
+++ init.el
@@ -101,7 +101,7 @@
   ;; init-evil dependent on init-clipboard
   (require 'init-clipboard)
   ;; use evil mode (vi key binding)
-  (require 'init-evil)
+  ;;(require 'init-evil)
   (require 'init-sh)
   (require 'init-ctags)
   (require 'init-bbdb)
