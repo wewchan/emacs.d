fd461eeefe6c5060de6a91b8fb026772e26492ef
wewchan <velly@cncd-av-vm-230-230.ccoe.lab.emc.com>
2017-05-12 17:51:35 +0800

    enable auto-complete, init go mode

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -1,11 +1,24 @@
+;; Set GO environment variable, add below sentences to .profile
+;; export GOROOT=/usr/local/go
+;; export GOPATH=$HOME/GOWS
+;; export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+;; 3rd party extensions
+;; Auto completion
+;; gocode
+;; go get -u github.com/nsf/gocode
+;; godef
+;; go get -u github.com/rogpeppe/godef
+;; errcheck
+;; go get -u github.com/kisielk/errcheck
+
 (add-hook 'before-save-hook' gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
-                         local-set-key (kbd "C-cC-r")'go-remove-unused-imports))
+                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c i") 'go-goto-imports)))
 
 (add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd \"M-.\") 'godef-jump)))
+                          (local-set-key (kbd  "M-.") 'godef-jump)))
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 
 (provide 'init-go-mode)
