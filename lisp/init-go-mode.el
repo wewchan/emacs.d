ac1b6db499f130beee05216a6a85eec1685e90fd
Velly <velly@cncd-av-vm-230-249.ccoe.lab.emc.com>
2017-09-25 14:50:07 +0800

    automatic setup env and utilities.

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -10,6 +10,32 @@
 ;; go get -u github.com/rogpeppe/godef
 ;; errcheck
 ;; go get -u github.com/kisielk/errcheck
+;; goguru
+;; go get golang.org/x/tools/cmd/guru
+;; go build golang.org/x/tools/cmd/guru
+;; mv guru $(go env GOROOT)/bin ​(for example)
+;; guru -help
+(defun set-go-environment-variable ()
+  "Set go workspace directory"
+  (interactive)
+  (message
+   (shell-command-to-string "mkdir -p $GOPATH")
+   )
+  )
+
+(defun set-go-toolchain ()
+  "Set go toolchains"
+  (interactive)
+  (message
+   (shell-command-to-string "go get -u github.com/nsf/gocode")
+   (shell-command-to-string "go get -u github.com/rogpeppe/godef")
+   (shell-command-to-string "go get -u github.com/kisielk/errcheck")
+   (shell-command-to-string "go get golang.org/x/tools/cmd/guru")
+   (shell-command-to-string "go build golang.org/x/tools/cmd/guru")
+   (shell-command-to-string "mv guru $GOROOT/bin")
+   (shell-command-to-string "guru --help")
+   )
+  )
 
 (add-hook 'before-save-hook' gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
