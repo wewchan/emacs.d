8d15aeb5b02c9e62fe4245ff22a2a0fd601bbdbf
wewchan <>
2017-11-02 02:42:22 -0700

    add more tools for golang support snippets

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -27,22 +27,32 @@
   "Set go toolchains"
   (interactive)
   (message
-   (shell-command-to-string "go get -u github.com/nsf/gocode")
-   (shell-command-to-string "go get -u github.com/rogpeppe/godef")
-   (shell-command-to-string "go get -u github.com/kisielk/errcheck")
-   (shell-command-to-string "go get golang.org/x/tools/cmd/guru")
-   (shell-command-to-string "go build golang.org/x/tools/cmd/guru")
-   (shell-command-to-string "mv guru $GOROOT/bin")
-   (shell-command-to-string "guru --help")
+   (shell-command-to-string "go get -u -v \
+    github.com/ramya-rao-a/go-outline \
+    github.com/acroca/go-symbols \
+    github.com/nsf/gocode \
+    github.com/rogpeppe/godef \
+    golang.org/x/tools/cmd/godoc \
+    github.com/zmb3/gogetdoc \
+    github.com/golang/lint/golint \
+    github.com/fatih/gomodifytags \
+    github.com/uudashr/gopkgs/cmd/gopkgs \
+    golang.org/x/tools/cmd/gorename \
+    sourcegraph.com/sqs/goreturns \
+    github.com/cweill/gotests/... \
+    golang.org/x/tools/cmd/guru \
+    github.com/josharian/impl")
+;;   (shell-command-to-string "go get -u github.com/nsf/gocode")
+;;   (shell-command-to-string "go get -u github.com/rogpeppe/godef")
+;;   (shell-command-to-string "go get -u github.com/kisielk/errcheck")
+;;   (shell-command-to-string "go get golang.org/x/tools/cmd/guru")
+;;   (shell-command-to-string "go build golang.org/x/tools/cmd/guru")
+;;   (shell-command-to-string "mv guru $GOROOT/bin")
+;;   (shell-command-to-string "guru --help")
    )
   )
 
 (add-hook 'before-save-hook' gofmt-before-save)
-(add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-(add-hook 'go-mode-hook (lambda ()
-                          (local-set-key (kbd "C-c i") 'go-goto-imports)))
-
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd  "M-.") 'godef-jump)))
 (add-hook 'go-mode-hook 'go-eldoc-setup)
