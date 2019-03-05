d3a4e08b5e7e26888a0454659d778372d0c828a1
U-CORP\chenv7 <chenv7@CNENCHENV7L2C.corp.emc.com>
2019-03-05 15:44:17 +0800

    Current works version on windows 10

--- lisp/init-plantuml.el
+++ lisp/init-plantuml.el
@@ -3,6 +3,8 @@
    (quote
     (("1.8.91" . "C:\\Program Files\\Java\\jdk1.8.0_91")))))
 
-(setq org-plantuml-jar-path (expand-file-name "~/.emacs.d/elpa/contrib/jar/plantuml.jar"))
+;(setq org-plantuml-jar-path (expand-file-name "~/.emacs.d/elpa/contrib/jar/plantuml.jar"))
+
+(setq org-plantuml-jar-path (expand-file-name "~/bin/plantuml.jar"))
 
 (provide 'init-plantuml)
